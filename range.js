4c29301fae07b1f7d857f45e0683aa4ff316289e
Waleed ur Rehman <waleedrehman31@gmail.com>
2021-05-04 15:01:28 +0500

    Inital commit

--- range.js
+++ range.js
@@ -1,4 +1,5 @@
 module.exports = (req, res, next) => {
-  res.header("Content-Range", "posts 0-20/0");
+  res.header("content-range", "Posts 0-20/0");
+  res.header("content-range", "Users 0-20/0");
   next();
 };
